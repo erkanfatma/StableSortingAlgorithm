8fc6be8b465eccc8e71383f4c8a0d5f195321137
Fatma Erkan <erkann.fatma@gmail.com>
2020-12-24 02:39:51 +0300

    initial commit.

---
+++ SelectionSortAlgorithm/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SelectionSortAlgorithm {
+      class Program {
+            static void Main(string[] args) {
+                  SelectionSort selectionSort = new SelectionSort();
+                  Stopwatch watch = new Stopwatch();
+
+                  int[] numbers = { 4, 7, 12, 5 ,3 ,2, 4, 1, 6, 22, 11, 10, 9, 23};
+
+                  Console.WriteLine("UNSTABLE SELECTION SORT");
+                  watch.Start();
+                  int[] unstableSort = selectionSort.UnstableSelectionSort(numbers);
+                  for (int i = 0; i < numbers.Length; i++) {
+                        Console.WriteLine(unstableSort[i] + " ");
+                  }
+                  watch.Stop();
+                  Console.WriteLine($"Elapsed Time: {watch.ElapsedMilliseconds} ms \n");
+
+                  Console.WriteLine("--------------------");
+
+                  Console.WriteLine("STABLE SELECTION SORT");
+                  watch.Restart();
+                  int[] stableSort = selectionSort.StableSelectionSort(numbers);
+                  for (int i = 0; i < numbers.Length; i++) {
+                        Console.WriteLine(stableSort[i]+ " ");
+                  }
+                  watch.Stop();
+                  Console.WriteLine($"Elapsed Time: {watch.ElapsedMilliseconds} ms \n");
+                  Console.Read();
+            }
+      }
+}
---
+++ SelectionSortAlgorithm/SelectionSort.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSortAlgorithm {
+      public class SelectionSort {
+            public int[] UnstableSelectionSort(int[] numberList) {
+                  int length = numberList.Length;
+
+                  for (int i = 0; i < length - 1; i++) {
+                        int min = i;
+                        for (int j = 0; j < length; j++) {
+                              if (numberList[j] < numberList[min])
+                                    min = j;
+                        }
+
+                        int temp = numberList[min];
+                        numberList[min] = numberList[i];
+                        numberList[i] = temp;
+                  }
+                  return numberList;
+            }
+
+            public int[] StableSelectionSort(int[] numberList) {
+                  int length = numberList.Length;
+
+                  for (int i = 0; i < length - 1; i++) {
+                        int min = i;
+                        for (int j = 0; j < length; j++) {
+                              if (numberList[min] > numberList[j])
+                                    min = j;
+                        }
+
+                        int temp = numberList[min];
+                        while(min > i) {
+                              numberList[min] = numberList[min - 1];
+                              min--;
+                        }
+                        numberList[i] = temp;
+                  }
+                  return numberList;
+            }
+      }
+}
